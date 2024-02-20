1e45973ce99d3918ff7baa3b20e840c73a5a267b
Shravan Bishnoi <116088301+shravanbishnoi@users.noreply.github.com>
2024-02-20 23:33:59 +0530

    Add files via upload

---
+++ JAVA/Code/Sorting Algo/Insertion Sort/InsertionSort.java
@@ -0,0 +1,46 @@
+/*
+ * Here is the implementation of Insertion sort algorithm
+ * for sorting a interger array
+ * 
+ * Author: Shravan
+ * Date: 20-02-2024
+ */
+
+
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.println("Original array:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
